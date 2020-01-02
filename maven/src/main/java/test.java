010288a993af2b6e87ed93b5316588dc5ec6025c
zhazeyu <2492762027@qq.com>
2020-01-02 15:04:51 +0800

    测试qqq

---
+++ maven/src/main/java/test.java
@@ -0,0 +1,11 @@
+/**
+ * @author zzy
+ * @autho/**
+ * @create 2020-01-02 15:03
+ */
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("测试qqqq");
+    }
+}
